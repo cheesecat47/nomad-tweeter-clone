70afa5a7f78a86235f1e7bdd4842faf434dc0c74
cheesecat47 <cheesecat47@gmail.com>
2021-02-22 17:35:54 +0900

    2.3

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,27 +1,40 @@
 import React, { useState } from 'react';
+import { authService } from '../fbase';
 
 const Auth = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
 
     const onChange = (event) => {
-        const {target: {name, value}} = event;
+        const { target: { name, value } } = event;
         if (name === "email") {
             setEmail(value);
         } else if (name === "password") {
             setPassword(value);
         }
     }
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" name="email" placeholder="Email" value={email} onChange={onChange} required />
+                <input type="email" name="email" placeholder="Email" value={email} onChange={onChange} required />
                 <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} required />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
 
             <div>
