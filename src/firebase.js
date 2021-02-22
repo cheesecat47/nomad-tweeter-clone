caa8b5db731667615adbfad89b692c1e8405b51f
cheesecat47 <cheesecat47@gmail.com>
2021-02-22 13:37:51 +0900

    1.1 keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,14 @@ import firebase from "firebase/app";
 // If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
 // import * as firebase from "firebase/app"
 
-import {
-    apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId
-} from "./.config/firebase.json";
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
