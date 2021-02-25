5e12f5da30d11ba5e81c7048abe988409d6322bc
cheesecat47 <cheesecat47@gmail.com>
2021-02-25 13:45:30 +0900

    Updates

---
+++ precache-manifest.21f1d4930564a0dd0d4fa2e084bf7f38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10c7a0bcfef96aae467d",
+    "url": "/nomad-tweeter-clone/static/css/main.ffca5fa7.chunk.css"
+  },
+  {
+    "revision": "10c7a0bcfef96aae467d",
+    "url": "/nomad-tweeter-clone/static/js/main.10c7a0bc.chunk.js"
+  },
+  {
+    "revision": "cb61770ec0f6a47ed43e",
+    "url": "/nomad-tweeter-clone/static/js/1.cb61770e.chunk.js"
+  },
+  {
+    "revision": "8d8b0e87126221d5eadf",
+    "url": "/nomad-tweeter-clone/static/js/runtime~main.8d8b0e87.js"
+  },
+  {
+    "revision": "f5e0e359b271180ee414296de048531d",
+    "url": "/nomad-tweeter-clone/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nomad-tweeter-clone/precache-manifest.21f1d4930564a0dd0d4fa2e084bf7f38.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nomad-tweeter-clone/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
